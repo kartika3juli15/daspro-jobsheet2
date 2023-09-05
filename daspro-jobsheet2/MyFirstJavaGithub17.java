9872400dbe1de2ea08546b36f3219d9bd9de7b66
kartika3juli15 <kartikatjuliana@gmail.com>
2023-09-05 14:22:43 +0700

    simpan kode java ke github

--- daspro-jobsheet2/MyFirstJavaGithub17.java
+++ daspro-jobsheet2/MyFirstJavaGithub17.java
@@ -1,5 +1,5 @@
-public class MyFirstJavaGithub17 {  
+public class MyFirstJavaGithub17 {
     public static void main(String[] args) {
         System.out.println("Hello From Github");
-    } 
+    }
 }
